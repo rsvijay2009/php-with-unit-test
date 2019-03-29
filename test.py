18d31620780c7325d89def7b8857b57990ee2e42
rsvijay2807 <vijayakumar.sivarasu@emishealth.com>
2019-03-29 16:43:01 +0530

    Test file

---
+++ test.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#
+# CLI script to find "is jenkins upto date?"
+#
+
+
+
+def get_jenkins_latest_release_version():
+   return "Yes"
+
+def get_current_installed_jenkins_version():
+    return "Yes"
+
+
+def main():
+    # Get Jenkins Latest version
+    latest_version = get_jenkins_latest_release_version()
+
+    # Get Installed Jenkins Version
+    installed_version = get_current_installed_jenkins_version()
+
+    if latest_version == installed_version:
+        print('You are alright!!!')
+    else:
+        print('Something went wrong!!!')
+
+if __name__ == "__main__":
+    # calling main function
+    main()
